78d3bd840e4ef7963e1cb7ee708ab84e8eea45e2
Bilol <bilolerkinboyev8@gmail.com>
2025-07-23 17:54:42 +0500

    Your update message here

--- lib/niners_game/logic/niners_logic.dart
+++ lib/niners_game/logic/niners_logic.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class NinersLogic with ChangeNotifier {
   List<bool> fields = [true, true, true, true, true, true, true, true, true];
+  bool isWinner = false;
 
   void change(int index) {
     fields[index] = !fields[index];
@@ -25,10 +26,28 @@ class NinersLogic with ChangeNotifier {
       index == 1
           ? fields[index + 3] = !fields[index + 3]
           : fields[index - 3] = !fields[index - 3];
-    } else if (index == 0 || index == 2 || index == 6 || index == 7) {
-      index == 1 || index == 7
-          ? fields[index + 1] = !fields[index + 1]
-          : fields[index - 1] = !fields[index - 1];
+    } else if (index == 0 || index == 2 || index == 6 || index == 8) {
+      if (index == 0 || index == 6) {
+        fields[index + 1] = !fields[index + 1];
+        if (index == 0) {
+          fields[index + 3] = !fields[index + 3];
+        }
+        if (index == 6) {
+          fields[index - 3] = !fields[index - 3];
+        }
+      }
+      if (index == 2 || index == 8) {
+        fields[index - 1] = !fields[index - 1];
+        if (index == 2) {
+          fields[index + 3] = !fields[index + 3];
+        }
+        if (index == 8) {
+          fields[index - 3] = !fields[index - 3];
+        }
+      }
+    }
+    if (!fields.contains(false)) {
+      isWinner = true;
     }
     notifyListeners();
   }
